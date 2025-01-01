5f4b1383c480187dd4fff38f01a5c87c22ae53b2
NAVEEN2422008 <70762589+NAVEEN2422008@users.noreply.github.com>
2025-01-01 16:04:19 +0530

    Add files via upload

--- solar-system.js
+++ solar-system.js
@@ -6,45 +6,47 @@ AFRAME.registerComponent('solar-system', {
     init: function() {
         this.setupConstants();
         this.setupElements();
-        this.createOrbits();
         this.setupEventListeners();
-    },
-
-    setupElements: function() {
-        this.celestialBodies = {};
-        ['sun', 'earth', 'moon', 'earthOrbit', 'moonOrbit'].forEach(id => {
-            const element = this.el.querySelector(`#${id}`);
-            if (element) {
-                this.celestialBodies[id] = element;
-            }
-        });
     },
 
     setupConstants: function() {
         this.constants = {
-            earthDistance: 2,
-            moonDistance: 0.8,
-            scales: {
-                sun: 0.8,
-                earth: 0.4,
-                moon: 0.15
+            distances: {
+                earth: 3,
+                moon: 0.8
             },
             speeds: {
-                earthRotation: 0.01,
-                earthOrbit: 0.005,
-                moonRotation: 0.01,
-                moonOrbit: 0.02,
-                sunRotation: 0.001
+                sun: 0.001,
+                earth: {
+                    rotation: 0.015,
+                    orbit: 0.005
+                },
+                moon: {
+                    rotation: 0.02,
+                    orbit: 0.015
+                }
             }
         };
         this.isVisible = false;
+    },
+
+    setupElements: function() {
+        this.elements = {};
+        ['sun', 'earth', 'moon', 'earthOrbit', 'moonOrbit'].forEach(id => {
+            const element = this.el.querySelector(`#${id}`);
+            if (element) {
+                this.elements[id] = element;
+            } else {
+                console.warn(`Missing element: ${id}`);
+            }
+        });
     },
 
     createOrbits: function() {
         // Create Earth's orbital path
         const earthPath = document.createElement('a-torus');
         earthPath.setAttribute('id', 'earthPath');
-        earthPath.setAttribute('radius', this.constants.earthDistance);
+        earthPath.setAttribute('radius', this.constants.distances.earth);
         earthPath.setAttribute('radius-tubular', '0.02');
         earthPath.setAttribute('segments-tubular', '50');
         earthPath.setAttribute('rotation', '90 0 0');
@@ -54,18 +56,18 @@ AFRAME.registerComponent('solar-system', {
         // Create Moon's orbital path
         const moonPath = document.createElement('a-torus');
         moonPath.setAttribute('id', 'moonPath');
-        moonPath.setAttribute('radius', this.constants.moonDistance);
+        moonPath.setAttribute('radius', this.constants.distances.moon);
         moonPath.setAttribute('radius-tubular', '0.01');
         moonPath.setAttribute('segments-tubular', '30');
         moonPath.setAttribute('rotation', '90 0 0');
         moonPath.setAttribute('material', 'color: #444; opacity: 0.3');
-        this.celestialBodies.earthOrbit.appendChild(moonPath);
+        this.elements.earthOrbit.appendChild(moonPath);
     },
 
     setupSystem: function() {
         // Sun setup
-        if (this.celestialBodies.sun) {
-            this.celestialBodies.sun.setAttribute('material', {
+        if (this.elements.sun) {
+            this.elements.sun.setAttribute('material', {
                 src: 'https://cdn.glitch.global/your-assets/sun.jpg',
                 emissive: '#FDB813',
                 emissiveIntensity: 0.7,
@@ -76,8 +78,8 @@ AFRAME.registerComponent('solar-system', {
         }
 
         // Earth setup
-        if (this.celestialBodies.earth) {
-            this.celestialBodies.earth.setAttribute('material', {
+        if (this.elements.earth) {
+            this.elements.earth.setAttribute('material', {
                 src: 'https://cdn.glitch.global/your-assets/earth.jpg',
                 normalMap: 'https://cdn.glitch.global/your-assets/earth_normal.jpg',
                 roughnessMap: 'https://cdn.glitch.global/your-assets/earth_rough.jpg',
@@ -85,26 +87,26 @@ AFRAME.registerComponent('solar-system', {
                 metalness: 0.2
             });
             this.setScale('earth');
-            this.celestialBodies.earth.object3D.position.set(this.constants.earthDistance, 0, 0);
+            this.elements.earth.object3D.position.set(this.constants.distances.earth, 0, 0);
         }
 
         // Moon setup
-        if (this.celestialBodies.moon) {
-            this.celestialBodies.moon.setAttribute('material', {
+        if (this.elements.moon) {
+            this.elements.moon.setAttribute('material', {
                 src: 'https://cdn.glitch.global/your-assets/moon.jpg',
                 normalMap: 'https://cdn.glitch.global/your-assets/moon_normal.jpg',
                 roughness: 0.9,
                 metalness: 0.1
             });
             this.setScale('moon');
-            this.celestialBodies.moon.object3D.position.set(this.constants.moonDistance, 0, 0);
+            this.elements.moon.object3D.position.set(this.constants.distances.moon, 0, 0);
         }
     },
 
     setScale: function(bodyName) {
         const scale = this.constants.scales[bodyName];
-        if (this.celestialBodies[bodyName] && scale) {
-            this.celestialBodies[bodyName].object3D.scale.set(scale, scale, scale);
+        if (this.elements[bodyName] && scale) {
+            this.elements[bodyName].object3D.scale.set(scale, scale, scale);
         }
     },
 
@@ -113,12 +115,12 @@ AFRAME.registerComponent('solar-system', {
         if (marker) {
             marker.addEventListener('markerFound', () => {
                 this.isVisible = true;
-                this.el.emit('solar-system-visible');
+                console.log('Solar system visible');
             });
             
             marker.addEventListener('markerLost', () => {
                 this.isVisible = false;
-                this.el.emit('solar-system-hidden');
+                console.log('Solar system hidden');
             });
         }
     },
@@ -128,25 +130,25 @@ AFRAME.registerComponent('solar-system', {
 
         const dt = deltaTime * 0.001 * this.data.timeScale;
 
-        // Rotate sun
-        if (this.celestialBodies.sun) {
-            this.celestialBodies.sun.object3D.rotation.y += this.constants.speeds.sunRotation * dt;
+        // Update rotations
+        if (this.elements.sun) {
+            this.elements.sun.object3D.rotation.y += this.constants.speeds.sun * dt;
         }
 
-        // Rotate and orbit Earth
-        if (this.celestialBodies.earthOrbit) {
-            this.celestialBodies.earthOrbit.object3D.rotation.y += this.constants.speeds.earthOrbit * dt;
-        }
-        if (this.celestialBodies.earth) {
-            this.celestialBodies.earth.object3D.rotation.y += this.constants.speeds.earthRotation * dt;
+        if (this.elements.earth) {
+            this.elements.earth.object3D.rotation.y += this.constants.speeds.earth.rotation * dt;
         }
 
-        // Rotate and orbit Moon
-        if (this.celestialBodies.moonOrbit) {
-            this.celestialBodies.moonOrbit.object3D.rotation.y += this.constants.speeds.moonOrbit * dt;
+        if (this.elements.earthOrbit) {
+            this.elements.earthOrbit.object3D.rotation.y += this.constants.speeds.earth.orbit * dt;
         }
-        if (this.celestialBodies.moon) {
-            this.celestialBodies.moon.object3D.rotation.y += this.constants.speeds.moonRotation * dt;
+
+        if (this.elements.moonOrbit) {
+            this.elements.moonOrbit.object3D.rotation.y += this.constants.speeds.moon.orbit * dt;
+        }
+
+        if (this.elements.moon) {
+            this.elements.moon.object3D.rotation.y += this.constants.speeds.moon.rotation * dt;
         }
     }
 });
